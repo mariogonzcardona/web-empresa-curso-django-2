e915660e2477518a836e7b4d9d87d91e094aa910
Mario Gonzalez <alejandrogonzalez@uadec.edu.mx>
2020-01-15 13:52:32 -0600

    First Commit Project Added

---
+++ webempresa/contact/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render,redirect
+from django.urls import reverse
+from .forms import ContactForm
+from django.core.mail import EmailMessage
+
+# Create your views here.
+def contact(request):
+    contact_form=ContactForm()
+    if request.method=="POST":
+        contact_form=ContactForm(data=request.POST)
+        if contact_form.is_valid():
+            name=request.POST.get("name","")
+            email=request.POST.get("email","")
+            content=request.POST.get("content","")
+            # Enviamos el correo y redireccionamos
+            email=EmailMessage(
+                "La caffettiera: Nuevo mensaje de contacto.",
+                "De: {} <{}>\n\nEscribio:\n\n{}".format(name,email,content),
+                "No contestar@inbox.mailtrap.io",
+                ["sanderbarraki@hotmail.com"],
+                reply_to=[email]
+            )
+            try:
+                # Todo ha ido bien
+                email.send()
+                return redirect(reverse("contact")+"?ok")
+            except:
+                # Algo no ha ido bien
+                return redirect(reverse("contact")+"?fail")
+            
+    return render(request,"contact/contact.html",{"form":contact_form})
